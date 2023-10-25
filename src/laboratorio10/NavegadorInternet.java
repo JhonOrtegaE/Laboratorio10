d8e0d08c8fcd6e238810ebfb84ab09a5a99899de
JhonOrtegaE <142622512+JhonOrtegaE@users.noreply.github.com>
2023-10-24 23:48:17 -0500

    Add files via upload

---
+++ src/laboratorio10/NavegadorInternet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package laboratorio10;
+
+/**
+ *
+ * @author Ortega Echenique
+ */
+public interface NavegadorInternet {
+    public void buscar();
+    public void Actualizar();
+    public void Detener();
+    public void Home();
+}
